3d87440ce6750ab27426d5129ef6c5da53d3b437
NguyenTrung26 <trungn.22it@vku.udn.vn>
2025-10-06 15:32:55 +0700

    fixpackage.json

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,5 +1,7 @@
 import { Suspense } from "react";
 import BookingClient from "./BookingClient";
+export const dynamic = "force-dynamic"
+export const revalidate = 0
 
 export default function BookingPage() {
   return (
